a584ad852844062608caea42af0da4123525a36a
Justin Slind <jslind@ualberta.ca>
2023-05-09 09:43:00 -0600

    script takes arguments, prep for build multi maps

--- mapdownloader.py
+++ mapdownloader.py
@@ -1,22 +1,22 @@
+import argparse
 import requests
 import os
+
 import shutil
 import sys
 import math
 from PIL import Image
+#from PIL.Image import core as _imaging
 
-
-x_start = 186
-x_end = 191
-
-y_start = 691
-y_end = 696
-
-zoom = 10
+parser = argparse.ArgumentParser(description='Download and Stitch WMS tiles')
+parser.add_argument('maps', nargs='+',
+                    help='eFIS map to process, ie: W118N50')
+parser.add_argument('-d','--download',action='store_true')
+parser.add_argument('--dryrun',action='store_true')
 
 def downloadmap():
-  for x in range(x_start,x_end):
-    for y in range(y_start,y_end):
+  for x in range(x_start,x_end+1):
+    for y in range(y_start,y_end+1):
       url = "https://cache.dciwx.com/basemaps/dci/vfr-canada/10/{}/{}.png".format(x,y)
       file = requests.get(url, stream=True)
       dump = file.raw
@@ -25,32 +25,78 @@ def downloadmap():
         shutil.copyfileobj(dump, location)
 
 def stitch():
-  new_im = Image.new('RGB', (256 * (x_end-x_start), 256 * (y_end-y_start)))
-  for x in range(x_start,x_end):
-    for y in range(y_start,y_end):
+  new_im = Image.new('RGB', (256 * (x_end+1-x_start), 256 * (y_end+1-y_start)))
+  print("stiching rows " + str(x_start) + " to " + str(x_end))
+  for x in range(x_start,x_end+1):
+    print("Row " + str(x),end=" ")
+    for y in range(y_start,y_end+1):
       image_file = "image-{}-{}.png".format(x,y)
-      print(image_file)
+      print(".",end='')
+      sys.stdout.flush()
       im=Image.open(image_file)
-      new_im.paste(im, (256 * (x-x_start),256 * (y_end-y-1)))
-  new_im.save('test.jpg')
+      new_im.paste(im, (256 * (x-x_start),256 * (y_end-y)))
+    print(" ")
+  new_im.save(map+".jpg")
 
+# From https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Tile_numbers_to_lon./lat._2
 def num2deg(xtile, ytile, zoom):
   n = 2.0 ** zoom
   lon_deg = xtile / n * 360.0 - 180.0
   lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
   lat_deg = math.degrees(lat_rad)
   return (lat_deg, lon_deg)
+#from https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Tile_numbers_to_lon./lat._2
+def deg2num(lat_deg, lon_deg, zoom):
+  lat_rad = math.radians(lat_deg)
+  n = 2.0 ** zoom
+  xtile = int((lon_deg + 180.0) / 360.0 * n)
+  ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+  return (xtile, ytile)
 
-stitch()
-(lat_deg, lon_deg) = num2deg(x_start,y_end,zoom)
-lat_deg_floor = math.floor(lat_deg)
-lat_deg_min = (lat_deg - lat_deg_floor) * 60
-lon_deg_floor = math.floor(lon_deg)
-lon_deg_min = (lon_deg - lon_deg_floor) * 60
-print("North West: ",lat_deg_floor, " ", lat_deg_min,",",lon_deg_floor," ",lon_deg_min)
-(lat_deg, lon_deg) = num2deg(x_end,y_start,zoom)
-lat_deg_floor = math.floor(lat_deg)
-lat_deg_min = (lat_deg - lat_deg_floor) * 60
-lon_deg_floor = math.floor(lon_deg)
-lon_deg_min = (lon_deg - lon_deg_floor) * 60
-print("South East: ",lat_deg_floor, " ", lat_deg_min,",",lon_deg_floor," ",lon_deg_min)
+
+args = parser.parse_args()
+print(args.maps, args.download, args.dryrun)
+
+for map in args.maps:
+
+  if map == "W108N50":
+      lonstart=-50.0
+      latstart=-116.0
+      lonend=-58.0
+      latend=-108.0
+  else:
+      print("Invalid map.")
+      quit()
+
+
+
+  x_start = 182
+  x_end = 204
+
+  y_start = 676
+  y_end = 715
+
+  zoom = 10
+
+  print(deg2num(lonstart,latstart,zoom))
+  print("should be " + str(x_start) + " to " + str(y_start))
+  
+  #downloadmap()
+  if (args.dryrun == False):
+    stitch()
+  (lat_deg, lon_deg) = num2deg(x_start,y_end,zoom)
+  lat_deg = abs(lat_deg)
+  lon_deg = abs(lon_deg)
+  lat_deg_floor = math.floor(lat_deg)
+  lat_deg_min = (lat_deg - lat_deg_floor) * 60
+  lon_deg_floor = math.floor(lon_deg)
+  lon_deg_min = (lon_deg - lon_deg_floor) * 60
+  print("North West: ",lat_deg_floor, " ", lat_deg_min,",",lon_deg_floor," ",lon_deg_min, "(",lat_deg,",",lon_deg,")")
+  (lat_deg, lon_deg) = num2deg(x_end,y_start,zoom)
+  lat_deg = abs(lat_deg)
+  lon_deg = abs(lon_deg)
+  lat_deg_floor = math.floor(lat_deg)
+  lat_deg_min = (lat_deg - lat_deg_floor) * 60
+  lon_deg_floor = math.floor(lon_deg)
+  lon_deg_min = (lon_deg - lon_deg_floor) * 60
+  print("South East: ",lat_deg_floor, " ", lat_deg_min,",",lon_deg_floor," ",lon_deg_min, "(",lat_deg,",",lon_deg,")")
